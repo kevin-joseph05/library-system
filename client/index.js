bdf1acaba6e143e6a5ed53668a7f6c186bad6ebe
kevin-joseph05 <148986958+kevin-joseph05@users.noreply.github.com>
2024-12-08 12:17:25 -0500

    Add files via upload

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './src/components/App.jsx'
+import "./public/styles.css";
+    
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
+
+
